96b7ae9869558f056b79f1dd6c2247917cc65f99
UFUOMA OGEDEGBE <ufuogbe@gmail.com>
2025-09-02 11:29:28 +0100

    database-adv-script/joins_queries.sql updated

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -8,7 +8,8 @@
 SELECT b.booking_id, b.property_id, b.start_date, b.end_date, u.user_id, u.name
 FROM bookings b
 INNER JOIN users u
-ON b.user_id = u.user_id;
+ON b.user_id = u.user_id
+ORDER BY b.booking_id;
 
 
 -- 2. 🧩 LEFT JOIN
@@ -16,7 +17,8 @@ ON b.user_id = u.user_id;
 SELECT p.property_id, p.property_name, r.review_id, r.comment, r.rating
 FROM properties p
 LEFT JOIN reviews r
-ON p.property_id = r.property_id;
+ON p.property_id = r.property_id
+ORDER BY p.property_id;
 
 
 -- 3. 🧩 FULL OUTER JOIN
@@ -26,4 +28,5 @@ ON p.property_id = r.property_id;
 SELECT u.user_id, u.name, b.booking_id, b.start_date, b.end_date
 FROM users u
 FULL OUTER JOIN bookings b
-ON u.user_id = b.user_id;
+ON u.user_id = b.user_id
+ORDER BY u.user_id, b.booking_id;
