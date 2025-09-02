aa2b3c766cff17e38ac04c18d1c7b3bebecb926e
UFUOMA OGEDEGBE <ufuogbe@gmail.com>
2025-09-02 09:36:45 +0100

    database-adv-script/perfomance.sql updated

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -36,3 +36,26 @@ CREATE INDEX idx_bookings_user_id ON bookings(user_id);
 CREATE INDEX idx_bookings_property_id ON bookings(property_id);
 CREATE INDEX idx_bookings_payment_id ON bookings(payment_id);
 CREATE INDEX idx_payments_status ON payments(status);
+EXPLAIN ANALYZE
+SELECT * FROM bookings b
+JOIN users u ON b.user_id = u.id
+JOIN properties p ON b.property_id = p.id
+JOIN payments pay ON b.payment_id = pay.id;
+SELECT 
+    b.id AS booking_id,
+    b.check_in_date,
+    b.check_out_date,
+    u.id AS user_id,
+    u.name AS user_name,
+    u.email,
+    p.id AS property_id,
+    p.title AS property_title,
+    p.location,
+    pay.id AS payment_id,
+    pay.amount,
+    pay.status
+FROM bookings b
+JOIN users u ON b.user_id = u.id
+JOIN properties p ON b.property_id = p.id
+JOIN payments pay ON b.payment_id = pay.id;
+
